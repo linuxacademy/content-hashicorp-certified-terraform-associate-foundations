ee6027189eab5e3d8ee8ad27f525c4c2d488ea3d
Moosa Khalid <moosa.khalid@linuxacademy.com>
2020-10-19 12:20:00 -0400

    Uploading HoL Section resource code

---
+++ section3-hol1/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "us-west-2"
+  region = "us-west-2"
+}
+
+
+resource "aws_sns_topic" "topic-us-east" {
+  provider = aws.us-east-1
+  name     = "topic-us-east"
+}
+
+resource "aws_sns_topic" "topic-us-west" {
+  provider = aws.us-west-2
+  name     = "topic-us-west"
+}
